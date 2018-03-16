b160bee32a7134082e281c99cfdc8f74fcfab796
dadoudi <hajar.erraoui@etu.upmc.fr>
2018-03-16 20:02:19 +0100

    app.scala

--- src/main/scala/fr/upmc/App.scala
+++ src/main/scala/fr/upmc/App.scala
@@ -1,6 +1,6 @@
 package fr.upmc
 
-import org.apache.spark.SparkConf
+import org.apache.spark.{SparkConf, SparkContext}
 
 /**
   * Hello world!
@@ -10,6 +10,12 @@ object App {
   def main(args: Array[String]) {
     var conf = new SparkConf()
     println("Hello World!")
+    conf.setMaster("local").setAppName("Analyse Loan Data")
+
+    val sc = new SparkContext(conf)
+    val rdd = sc.textFile("loan0.csv")
+
+    println(rdd.count)
   }
 
 }
