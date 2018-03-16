f9c57d9e43e9130afbc80bd20fe251e75ef1dc0a
3674435 <mohamed.kassar@etu.upmc.fr>
2018-03-16 12:15:31 -0400

    project init

---
+++ src/main/scala/upmc/fr/App.scala
@@ -0,0 +1,15 @@
+package upmc.fr
+
+import org.apache.spark.SparkConf
+
+/**
+  * Hello world!
+  *
+  */
+object App {
+  def main(args: Array[String]) {
+    var conf = new SparkConf()
+    println("Hello World!")
+  }
+
+}
