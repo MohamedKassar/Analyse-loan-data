01520830731f9c78ed449f93fd37ff0221d6acfb
3674435 <mohamed.kassar@etu.upmc.fr>
2018-03-25 18:04:36 -0400

    kmeans

--- src/main/scala/fr/upmc/App.scala
+++ src/main/scala/fr/upmc/App.scala
@@ -1,6 +1,8 @@
 package fr.upmc
 
 
+import java.io.FileWriter
+
 import org.apache.spark._
 import org.apache.spark.mllib.clustering.{KMeans, KMeansModel}
 import org.apache.spark.mllib.linalg.Vectors
@@ -20,10 +22,8 @@ object App {
 
   def main(args: Array[String]) {
 
-    val conf = new SparkConf()
-    conf.setMaster("local").setAppName("Analyse Loan Data")
-
-    val kk = new KMeans()
+    //val conf = new SparkConf()
+    //conf.setMaster("local[2]").setAppName("Analyse Loan Data")
 
 
     //val sc = new SparkContext(conf)
@@ -32,6 +32,8 @@ object App {
       .builder()
       .appName("Analyse Loan Data").master("local[2]")
       .getOrCreate()
+
+    sqlContext.sparkContext.setLogLevel("ERROR")
 
     println("Hello World!")
 
@@ -55,35 +57,68 @@ object App {
       .withColumnRenamed("open_acc", "open_account")
       .withColumnRenamed("pub_rec", "public_records")
     //.withColumnRenamed("open_acc", "open_account").limit(100).show()
-
-    printf("5000.0".toDouble.toString)
+    // useless : issue_d - pymnt_plan - url - desc - addr_state
+    // recheck : earliest_cr_line - mths_since_last_delin - mths_since_last_record - initial_list_status
+    println(cleanDF.count())
     val header = cleanDF.first
     cleanDF = cleanDF.filter(l => header != l)
     import sqlContext.implicits._
-    import sqlContext._
-    val cachedData = cleanDF.select("loan_amount", "annual_income").where("annual_income < 500000").map(_.toString) /*.map(s=> {
-        val ss = s.replaceAll("\\[","").replaceAll("\\]","").split(",");
-        Array(ss(0).toDouble, ss(1).toDouble)
-        })*/ .toDF().rdd.map(l => {
-
+    val cachedData = cleanDF.select("dti", "delinquent_2_years", "inquiries_last_6_months",
+      "zip_code", "home_ownership", "grade", "installment", "interest_rate", "term", "loan_amount", "funded_amount",
+      "investor_funds", "sub_grade", "emp_title", "emp_length", "annual_income", "verification_status", "purpose", "title",
+      "open_account", "public_records", "revol_bal", "revol_util", "total_acc") /*.where("dti < 500000")*/ .map(_.toString)
+      .toDF().rdd.map(l => {
       val ss = l.toString().replaceAll("\\[", "").replaceAll("\\]", "").split(",");
 
-      var a: Double = -1
-      var b: Double = -1
       try {
-        a = ss(0).toDouble
-        b = ss(1).toDouble
-      }catch {
-        case _ => a = -1; b = -1
+        var i = -1
+
+        def I = {
+          i = i + 1;
+          i
+        }
+
+        def convertS(s: String) = {
+          if (s == null) "unknown".hashCode else s.trim.toLowerCase.hashCode
+        }
+
+        def convertD(s: String) = {
+          s.toDouble
+        }
+
+        Vectors.dense(
+          convertD(ss(I)), convertD(ss(I)), convertD(ss(I)), convertD(ss(I).substring(0, 3)), convertS(ss(I)),
+          convertS(ss(I)), convertS(ss(I)), convertS(ss(I)), convertS(ss(I)), convertD(ss(I)), convertD(ss(I)), convertD(ss(I)),
+          convertS(ss(I)), convertS(ss(I)), convertS(ss(I)), convertD(ss(I)), convertS(ss(I)), convertS(ss(I)), convertS(ss(I)),
+          convertD(ss(I)), convertD(ss(I)), convertD(ss(I)), convertD(ss(I)), convertD(ss(I))
+        )
+      } catch {
+        case _ => Vectors.dense(-1)
       }
-      Vectors.dense(a/10000, b/10000)
-    }).filter(l=>l(0) != -1 && l(1) != -1).cache()
+    }).filter(l => l(0) != -1).cache()
 
     println(cachedData.count())
+    var x = 1
+    val fw = new FileWriter("test.txt", true)
 
-    val clusters = KMeans.train(cachedData, 6, 20)
-
+    for (x <- 2 to 10) {
+      println("---------------- k = " + x + "  -------------")
+      val clusters = KMeans.train(cachedData, x, 20)
+      clusters.clusterCenters.foreach(println)
+      val WSSSE = clusters.computeCost(cachedData)
+      println("---------------- WSSSE = " + WSSSE + "  -------------")
+      try {
+        fw.write("\nk = " + x + "\nWSSSE = " + WSSSE)
+        fw.flush()
+      }
+    }
+    fw.close()
+    /*
+    val clusters = KMeans.train(cachedData, 2, 20)
     clusters.clusterCenters.foreach(println)
+    val WSSSE = clusters.computeCost(cachedData)
+    */
+    //println(clusters.predict(Vectors.dense(1.0, 4.0)))
     //Vectors.dense(s.map(_.toString.toDouble).toArray)
 
 
